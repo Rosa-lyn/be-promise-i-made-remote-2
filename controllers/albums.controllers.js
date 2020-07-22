d66e81a86f937266e230f70fdea93fa8f737c990
Rosalyn Land <roz.s.land@gmail.com>
2020-07-22 12:33:06 +0100

    completed fetchAllAlbums

--- controllers/albums.controllers.js
+++ controllers/albums.controllers.js
@@ -4,8 +4,23 @@ const { fetchAllAlbums } = require("../models/albums.models.js");
 
 const getAllAlbums = (req, res) => {
   fetchAllAlbums().then((albums) => {
-    res.send(albums);
+    res.send({ albums });
   });
 };
 
 module.exports = { getAllAlbums };
+/*
+{
+  "albums": [
+    {
+      "title": "10 Years of Akala",
+      "artwork": "http://images.genius.com/bd174bbb42265536ee1741370f5dc593.600x600x1.jpg"
+    },
+    {
+      "title": "I Am... Sasha Fierce",
+      "artwork": "https://upload.wikimedia.org/wikipedia/en/9/96/I_Am..._Sasha_Fierce.png"
+    },
+    ...
+  ]
+}
+*/
--- models/albums.models.js
+++ models/albums.models.js
@@ -4,7 +4,20 @@ const fetchAllAlbums = () => {
   return axios
     .get("https://nc-spotify-jams.herokuapp.com/api/albums")
     .then((result) => {
-      return result.data;
+      const formattedAlbums = [];
+      result.data.albums.forEach((album) => {
+        const returnAlbum = {};
+        returnAlbum.title = album.title;
+        returnAlbum.artwork = album.artwork;
+        formattedAlbums.push(returnAlbum);
+      });
+      return formattedAlbums;
+    })
+    .catch((err) => {
+      let error = {};
+      error.status = err.response.status;
+      error.msg = err.response.statusText;
+      console.log({ error });
     });
 };
 
